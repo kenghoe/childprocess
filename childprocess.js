2f746e1f0f85c7adf2aed18192acb118c1a5a3bd
TAN KENG HOE <10207549@INFOSOFT.STELECT.LOCAL>
2017-03-09 15:27:45 +0800

    add express

--- childprocess.js
+++ childprocess.js
@@ -5,14 +5,32 @@ const debug = {
 const 
 	Logger =				require('tracer').colorConsole({level: debug.level}),
 	ChildProcessExec =		require('child_process').exec,
+	Express =				require('express'),
+	ExpressHandlebars =		require('express-handlebars'),
 	_ = null;
 
+var express = Express();
 
-Logger.info("call ChildProcessExec('my.bat') !!");
-ChildProcessExec('my.bat', (err, stdout, stderr) => {  
-  if (err) {  
-    Logger.error("err - '%s'", err);  
-    return;  
-  }  
-  Logger.info("stdout - '%s'", stdout);  
-});  
+Logger.info("register template engine for 'handlebars' extension !!");
+express.engine('handlebars', ExpressHandlebars({defaultLayout: 'main'}));
+
+Logger.info("set 'handlebars' template engine !!");
+express.set('view engine', 'handlebars');
+
+Logger.info("set GET '/my-bat' route !!");
+express.get('/my-bat', function (req, res) {
+	Logger.info("call ChildProcessExec('my.bat') !!");
+	ChildProcessExec('my.bat', (err, stdout, stderr) => {
+		if (err) {  
+			Logger.error("err - '%s'", err);  
+			res.send({err: err});
+			return;  
+		}  
+		Logger.info("stdout - '%s'", stdout);
+		res.send({stdout : stdout});
+	});  
+});
+
+Logger.info("listen port 3000 !!");
+express.listen(3000);
+
